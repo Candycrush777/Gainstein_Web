5ff847941a9fda4e324d66d11748051e69325322
Eduardo Afán <eduardoafan.eae@gmail.com>
2025-10-25 13:50:33 +0200

    vuetify only necesary imports

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -1,12 +1,61 @@
 import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
+import {
+  VApp,
+  VMain,
+  VContainer,
+  VRow,
+  VCol,
+  VCard,
+  VBtn,
+  VTextField,
+  VSwitch,
+  VDialog,
+  VSnackbar,
+  VAlert,
+  VPagination,
+  VNavigationDrawer,
+  VIcon,
+  VDivider,
+  VList,
+  VListItem,
+  VCardTitle,
+  VCardText,
+  VCardActions
+} from 'vuetify/components'
+import { Ripple, Scroll, Intersect, Touch } from 'vuetify/directives'
 
 export default defineNuxtPlugin(nuxtApp => {
   const vuetify = createVuetify({
     ssr: true,
-    components,
-    directives,
+    components: {
+      VApp,
+      VMain,
+      VContainer,
+      VRow,
+      VCol,
+      VCard,
+      VBtn,
+      VTextField,
+      VSwitch,
+      VDialog,
+      VSnackbar,
+      VAlert,
+      VPagination,
+      VNavigationDrawer,
+      VIcon,
+      VDivider,
+      VList,
+      VListItem,
+      VCardTitle,
+      VCardText,
+      VCardActions
+    },
+    directives: {
+      Ripple,
+      Scroll,
+      Intersect,
+      Touch
+    }
   })
   nuxtApp.vueApp.use(vuetify)
 })
